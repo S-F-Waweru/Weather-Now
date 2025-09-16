b0b92a9b4c4d23bc2a5c7613cff00dac3eeecfc7
Samuel F. W. Ndegwa <s.f.waweru@gmail.com>
2025-09-16 08:36:21 +0300

    Add offline component with basic structure, styles, and tests; adjust home component styles for consistency

---
+++ src/app/View/offline/offline.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offline',
+  imports: [],
+  templateUrl: './offline.html',
+  styleUrl: './offline.css'
+})
+export class Offline {
+
+}
